24174f5e252af73212a24357a545c394fa4c3d79
ruda@fedora <hindrikson@protonmail.com>
2024-02-07 20:09:13 +0100

    updates

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -2,10 +2,10 @@ return {
     "lervag/vimtex",
     config = function()
         -- for linux
-        -- vim.g.vimtex_view_method = 'zathura' -- SumatraPDF for windows
+        vim.g.vimtex_view_method = 'zathura' -- SumatraPDF for windows
         -- for windows
-        vim.g.vimtex_view_general_viewer = '~/.local/bin/sumatrapdf.sh' -- SumatraPDF for windows
-        vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
+        -- vim.g.vimtex_view_general_viewer = '~/.local/bin/sumatrapdf.sh' -- SumatraPDF for windows
+        -- vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
 
 
         vim.g.vimtex_compiler_method = 'latexmk'
