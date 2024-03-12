5009e41a2fba05f27a36e1a8d1ed38ada2353ee4
Ruda <hindrikson@proton.me>
2024-03-12 15:03:56 +0100

    updates

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -4,11 +4,16 @@ return {
     --     name = "catpuccin",
     --     priority = 1000,
     -- },
-    -- {
-    --     "rose-pine/neovim",
-    --     name = "rose-pine",
-    --     priority = 1000,
-    -- },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        priority = 1000,
+        filetypes = {"markdown"},
+        config = function()
+            vim.g.rose_pine_variant = "moon"
+            vim.cmd.colorscheme "rose-pine-moon"
+        end
+    },
     {
         "folke/tokyonight.nvim",
         lazy = false,
